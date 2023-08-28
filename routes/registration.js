d52be48254c5d42cae6feb86d0c40306d4256436
KayDrew <kdtshehla@gmail.com>
2023-08-28 07:37:02 -0700

    formatted code

--- routes/registration.js
+++ routes/registration.js
@@ -1,96 +1,98 @@
 
 export default function regNumbers(queries) {
-	let regex=  /[a-zA-Z][\s][1-9]/g;
-	let regex1=/[a-zA-Z][\s][1-9][-][1-9]/g;
-	let regex2=/[a-zA-Z][\s][1-9][\s][1-9]/g;
-	let error="";
-	async function getRegTown(req,res, next){
 
-let code= req.params.reg_numbers;
+	//test 3 string format cases
+	let regex = /[a-zA-Z][\s][1-9]/g;
+	let regex1 = /[a-zA-Z][\s][1-9][-][1-9]/g;
+	let regex2 = /[a-zA-Z][\s][1-9][\s][1-9]/g;
+	let error = "";
+	async function getRegTown(req, res, next) {
 
-try{
-	let townReg=await queries.getTown(code);
-	
-	res.redirect("/");
-	
-}catch(err){
-	console.log(err);
-}
-}
+		let code = req.params.reg_numbers;
 
-async function getAllTowns(req,res, next){
-	
-	try{
-	let allReg=await queries.getAll();
-	
-	res.redirect("/");
-	
-}catch(err){
-	console.log(err);
-}
-}
+		try {
+			let townReg = await queries.getTown(code);
 
-async function recordRegNum(req,res, next){
+			res.redirect("/");
 
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-	let reg1= req.body.reg;
-	let reg=reg1.trim().toUpperCase();
+	async function getAllTowns(req, res, next) {
 
-	let code= reg[0]+reg[1];
-	
-	if(reg){
+		try {
+			let allReg = await queries.getAll();
 
-if((regex.test(reg) || regex1.test(reg) || regex2.test(reg)) && (reg.length>=6 && reg.length<=20)){
+			res.redirect("/");
 
-if(reg[0]=="C" && (reg[1]=="A" || reg[1]=="J" || reg[1]=="Y")){
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-error="";
-
-}
-
-else{
-error="Only registration numbers for Cape Town,Paarl & Bellville";
-reg="";
-
-}
-
-}
-
-else{
-	
-	error="Please enter a valid registration number";
-	reg="";
-}
-
-}
-
-else{
-
-error="Please enter a registration number";
-reg="";
-}
-
-req.flash("error",error);
-		
-	try{
-	await queries.recordReg(reg,code);
-	
-	res.render("index",{
-});
-	
-}catch(err){
-	console.log(err);
-}
+	async function recordRegNum(req, res, next) {
 
 
-}
+		let reg1 = req.body.reg;
+		let reg = reg1.trim().toUpperCase();
 
-return{
-getRegTown,
-getAllTowns,
-recordRegNum 
+		let code = reg[0] + reg[1];
 
-}
+		if (reg) {
+
+			if ((regex.test(reg) || regex1.test(reg) || regex2.test(reg)) && (reg.length >= 6 && reg.length <= 20)) {
+
+				if (reg[0] == "C" && (reg[1] == "A" || reg[1] == "J" || reg[1] == "Y")) {
+
+					error = "";
+
+				}
+
+				else {
+					error = "Only registration numbers for Cape Town,Paarl & Bellville";
+					reg = "";
+
+				}
+
+			}
+
+			else {
+
+				error = "Please enter a valid registration number";
+				reg = "";
+			}
+
+		}
+
+		else {
+
+			error = "Please enter a registration number";
+			reg = "";
+		}
+
+		req.flash("error", error);
+
+		try {
+			await queries.recordReg(reg, code);
+
+			res.render("index", {
+			});
+
+		} catch (err) {
+			console.log(err);
+		}
+
+
+	}
+
+	return {
+		getRegTown,
+		getAllTowns,
+		recordRegNum
+
+	}
 
 
 }
