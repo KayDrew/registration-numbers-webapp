3d1f7630dda1c449b554ccd1edef09ff21e66008
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-08-29 21:18:12 +0200

    Update registration.js

--- routes/registration.js
+++ routes/registration.js
@@ -1,112 +1,145 @@
-
 export default function regNumbers(queries) {
+	
+	let regex=  /[a-zA-Z][\s][1-9]/;
+	let regex1=/[a-zA-Z][\s][1-9][-][1-9]/;
+	let regex2=/[a-zA-Z][\s][1-9][\s][1-9]/;
+	let towns=[];
+	let regTown="";
+	let error="";
+	
+   async function homeRoute(req, res,next) {
+  
+       let towns=await getRegTown();
 
-	//test 3 string format cases
-	let regex = /[a-zA-Z][\s][1-9]/;
-	let regex1 = /[a-zA-Z][\s][1-9][-][1-9]/;
-	let regex2 = /[a-zA-Z][\s][1-9][\s][1-9]/;
-	let error = "";
-	async function getRegTown(req, res, next) {
+          res.render("index",{
+          regNums:towns 
+      });
 
-		let code = req.params.reg_numbers;
-
-		try {
-			let townReg = await queries.getTown(code);
-
-			res.redirect("/");
-
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	async function getAllTowns(req, res, next) {
-
-		try {
-			let allReg = await queries.getAll();
-
-			res.redirect("/");
-
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	async function recordRegNum(req, res, next) {
+    }
 
 
-		let reg1 = req.body.reg;
-		let reg = reg1.trim().toUpperCase();
+	async function getRegTown(req,res, next){
 
-		let code = reg[0] + reg[1];
+         try{
+	
+	              if(regTown=="All" ||regTown==""){
 
-		if (reg) {
+                        towns=await queries.getAll();
 
-			if ((regex.test(reg) || regex1.test(reg) || regex2.test(reg)) && (reg.length >= 6 && reg.length <= 20)) {
+                  }
 
-				if (reg[0] == "C" && (reg[1] == "A" || reg[1] == "J" || reg[1] == "Y")) {
+              else{
 
-					let result=await queries.getAll();
+                        towns= await queries.getTown(regTown);
+   
+                }
+  
+              return towns;
+	          res.redirect("/");
+	
+       }catch(err){
+	                console.log(err);
+         }
+}
 
-                                         for(let i=0;i<result.length;++i){
 
-                                                if(reg==result[i].regnumber){
 
-                                                       error="This registration number already exists";
-                                                       reg="";
-                                                       break;
+   async function recordRegNum(req,res, next){
+
+         let town=req.body.town;
+	    var reg1= req.body.reg;
+	    var reg=reg1.trim().toUpperCase();
+	    let code= reg[0]+reg[1];
+	
+	   if(reg){
+
+                     if((regex.test(reg) || regex1.test(reg) || regex2.test(reg)) && (reg.length>=6 && reg.length<=30)){
+
+                                  if(reg[0]=="C" && (reg[1]=="A" || reg[1]=="J" || reg[1]=="Y")){
+
+                                                let result=await queries.getAll();
+
+                                               for(let i=0;i<result.length;++i){
+
+                                                          if(reg==result[i].regnumber){
+
+                                                                       error="This registration number already exists";
+                                                                       reg="";
+                                                                       break;
+                                                              }
+
+                                                             else{
+                                                                       error="";
+                                                                  }
                                                  }
+		
+                                       }
 
-                                                 else{
-                                                        error="";
-                                                  }
-                                         }
+                                    else{
+                                                  error="Only registration numbers for Cape Town,Paarl & Bellville";
+                                                  reg="";
+                                       }
 
-				}
+                       }
 
-				else {
-					error = "Only registration numbers for Cape Town,Paarl & Bellville";
-					reg = "";
+                       else{
+	                                error="Please enter a valid registration number";
+	                                reg="";
+                           }
 
-				}
+            }
 
-			}
+        else{
+                     error="Please enter a registration number";
+                     reg="";
+            }
 
-			else {
-
-				error = "Please enter a valid registration number";
-				reg = "";
-			}
-
-		}
-
-		else {
-
-			error = "Please enter a registration number";
-			reg = "";
-		}
-
-		req.flash("error", error);
-
-		try {
-			await queries.recordReg(reg, code);
-
-			res.render("index", {
-			});
-
-		} catch (err) {
-			console.log(err);
-		}
+        req.flash("error",error);
 
 
-	}
+        try{
+	           await queries.recordReg(reg,code);
+	           towns= getRegTown();
+	           res.redirect("/");
+	
+            }catch(err){
+	          console.log(err);
+            }
 
-	return {
-		getRegTown,
-		getAllTowns,
-		recordRegNum
 
-	}
+  }
+
+
+  async function  deleteAll(req,res,next){
+
+            try{
+
+                        await queries.deleteData();
+                        res.redirect("/");
+                  }catch(err){
+                       console.log(err);
+
+                 }
+
+   }
+
+async function show(req,res,next){
+
+               regTown= req.body.town;
+               towns=getRegTown();
+               res.redirect("/");
+  }
+
+
+
+  return{
+  getRegTown,
+  recordRegNum,
+  deleteAll,
+  show,
+  homeRoute
+
+  }
 
 
 }
